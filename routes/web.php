ddbcb62bbd10c4a338546d4e99a2815a839138c0
samsep1ol <99366403+samsep1ol-dev@users.noreply.github.com>
2025-05-25 21:43:49 -0300

    Criando rotas personalizadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+//usar o modulo roto
 use Illuminate\Support\Facades\Route;
 
+// onde é definido a rota
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+//minha rota fds
+Route::get('/b', function(){
+    echo 'random';
 });
